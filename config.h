e7c781534a7d886682fd7c9fc0c17ae327a57a29
A1phaZer0 <ini.universe@gmail.com>
2016-07-30 21:17:35 +0800

    update config.h

--- config.h
+++ config.h
@@ -28,12 +28,13 @@
 #define MINWSZ          50        /* minimum window size in pixels */
 #define DEFAULT_MONITOR 0         /* the monitor to focus initially */
 #define DEFAULT_DESKTOP 0         /* the desktop to focus initially */
-#define DESKTOPS        5         /* number of desktops - edit DESKTOPCHANGE keys to suit */
-#define USELESSGAP      8         /* the size of the useless gap in pixels */
+#define DESKTOPS        4         /* number of desktops - edit DESKTOPCHANGE keys to suit */
+#define USELESSGAP      8         /* the size of the useless gap in pixels */                        
 #define COVER_PANEL     False     /* full screen window covers panel or not */
 #define MONOCLE_GAP     False     /* useless gap in monocle mode
                                    * set MONOCLE_BORDER to **True** before using this*/
 #define MONOCLE_BORDER  False     /* window border in monocle mode */
+
 
 struct ml {
     int m; /* monitor that the desktop in on  */
@@ -59,15 +60,7 @@ struct ml {
 static const struct ml init[] = { \
     /* monitor  desktop   mode  masz  sbar   */
     {     0,       0,   { TILE,  50,  True  } },
-    {     0,       1,   { FLOAT, 50,  True  } },
-    {     0,       2,   { FLOAT, 50,  True  } },
-    {     0,       3,   { FLOAT, 50,  True  } },
-    {     0,       4,   { FLOAT, 50,  True  } },
-    {     1,       0,   { GRID,  50,  False } },
-    {     1,       1,   { FLOAT, 50,  False } },
-    {     1,       2,   { FLOAT, 50,  False } },
-    {     1,       3,   { FLOAT, 50,  False } },
-    {     1,       4,   { FLOAT, 50,  False } },
+    {     1,       2,   { GRID,  0,   False } },
 };
 
 /**
@@ -79,7 +72,7 @@ static const struct ml init[] = { \
 static const AppRule rules[] = { \
     /*  class     monitor  desktop  follow  float */
     { "MPlayer",     0,       3,    True,   False },
-    { "Gimp",        0,       2,    False,  True  },
+    { "Gimp",        1,       0,    False,  True  },
 };
 
 /* helper for spawning shell commands */
@@ -90,8 +83,6 @@ static const AppRule rules[] = { \
  * must always end with ', NULL };'
  */
 static const char *termcmd[] = { "urxvt", NULL };
-// static const char *menucmd[] = { "dmenu_run", "-dim", "0.6", "-fn", "-gohu-gohufont-medium-r-normal--14-100-100-100-c-80-iso10646-1", "-h", "22", "-l", "16", "-w", "200", "-sb", "#823232", NULL };
-static const char *menucmd[] = { "dmenu_run", "-dim", "0.6", "-fn", "monofur for Powerline-13:bold", "-h", "24", "-l", "16", "-w", "200", "-sb", "#3f3f3f", "-centerx", "-s", "0", NULL };
 
 #define MONITORCHANGE(K,N) \
     {  MOD4,             K,              change_monitor, {.i = N}}, \
@@ -107,9 +98,8 @@ static const char *menucmd[] = { "dmenu_run", "-dim", "0.6", "-fn", "monofur for
 static Key keys[] = {
     /* modifier          key            function           argument */
     {  MOD1,             XK_b,          togglepanel,       {NULL}},
-    {  MOD1,             XK_Delete,     focusurgent,       {NULL}},
-    {  MOD1|MOD4,        XK_c,          killclient,        {NULL}},
-    {  MOD4,             XK_c,          centerwindow,      {NULL}},
+    {  MOD1,             XK_BackSpace,  focusurgent,       {NULL}},
+    {  MOD1|SHIFT,       XK_c,          killclient,        {NULL}},
     {  MOD1,             XK_j,          next_win,          {NULL}},
     {  MOD1,             XK_k,          prev_win,          {NULL}},
     {  MOD1,             XK_h,          resize_master,     {.i = -10}}, /* decrease size in px */
@@ -118,44 +108,34 @@ static Key keys[] = {
     {  MOD1,             XK_p,          resize_stack,      {.i = +10}}, /* grow     size in px */
     {  MOD1|CONTROL,     XK_h,          rotate,            {.i = -1}},
     {  MOD1|CONTROL,     XK_l,          rotate,            {.i = +1}},
-    {  MOD1|MOD4,        XK_h,          rotate_filled,     {.i = -1}},
-    {  MOD1|MOD4,        XK_l,          rotate_filled,     {.i = +1}},
+    {  MOD1|SHIFT,       XK_h,          rotate_filled,     {.i = -1}},
+    {  MOD1|SHIFT,       XK_l,          rotate_filled,     {.i = +1}},
     {  MOD1,             XK_Tab,        last_desktop,      {NULL}},
     {  MOD1,             XK_Return,     swap_master,       {NULL}},
-    {  MOD1|MOD4,        XK_j,          move_down,         {NULL}},
-    {  MOD1|MOD4,        XK_k,          move_up,           {NULL}},
-    {  MOD1|MOD4,        XK_6,          switch_mode,       {.i = TILE}},
-    {  MOD1|MOD4,        XK_7,          switch_mode,       {.i = MONOCLE}},
-    {  MOD1|MOD4,        XK_9,          switch_mode,       {.i = BSTACK}},
-    {  MOD1|MOD4,        XK_0,          switch_mode,       {.i = GRID}},
-    {  MOD1|MOD4,        XK_8,          switch_mode,       {.i = FLOAT}},
+    {  MOD1|SHIFT,       XK_j,          move_down,         {NULL}},
+    {  MOD1|SHIFT,       XK_k,          move_up,           {NULL}},
+    {  MOD1|SHIFT,       XK_t,          switch_mode,       {.i = TILE}},
+    {  MOD1|SHIFT,       XK_m,          switch_mode,       {.i = MONOCLE}},
+    {  MOD1|SHIFT,       XK_b,          switch_mode,       {.i = BSTACK}},
+    {  MOD1|SHIFT,       XK_g,          switch_mode,       {.i = GRID}},
+    {  MOD1|SHIFT,       XK_f,          switch_mode,       {.i = FLOAT}},
     {  MOD1|CONTROL,     XK_r,          quit,              {.i = 0}}, /* quit with exit value 0 */
     {  MOD1|CONTROL,     XK_q,          quit,              {.i = 1}}, /* quit with exit value 1 */
     {  MOD1|SHIFT,       XK_Return,     spawn,             {.com = termcmd}},
-    {  MOD4,             XK_m,          spawn,             {.com = menucmd}},
-    {  MOD4,             XK_j,          moveresize,        {.v = (int []){   0,  2,   0,   0 }}}, /* move down  */
-    {  MOD4,             XK_k,          moveresize,        {.v = (int []){   0, -2,   0,   0 }}}, /* move up    */
-    {  MOD4,             XK_l,          moveresize,        {.v = (int []){  2,   0,   0,   0 }}}, /* move right */
-    {  MOD4,             XK_h,          moveresize,        {.v = (int []){ -2,   0,   0,   0 }}}, /* move left  */
-    {  CONTROL|SHIFT,    XK_j,          moveresize,        {.v = (int []){   0,   0,   0,  2 }}}, /* height grow   */
-    {  CONTROL|SHIFT,    XK_k,          moveresize,        {.v = (int []){   0,   0,   0, -2 }}}, /* height shrink */
-    {  CONTROL|SHIFT,    XK_l,          moveresize,        {.v = (int []){   0,   0,  2,   0 }}}, /* width grow    */
-    {  CONTROL|SHIFT,    XK_h,          moveresize,        {.v = (int []){   0,   0, -2,   0 }}}, /* width shrink  */
-    {  MOD4|SHIFT,       XK_j,          moveresize,        {.v = (int []){   0,  20,  0,   0 }}}, /* move down  */
-    {  MOD4|SHIFT,       XK_k,          moveresize,        {.v = (int []){   0, -20,  0,   0 }}}, /* move up    */
-    {  MOD4|SHIFT,       XK_l,          moveresize,        {.v = (int []){  20,  0,   0,   0 }}}, /* move right */
-    {  MOD4|SHIFT,       XK_h,          moveresize,        {.v = (int []){ -20,  0,   0,   0 }}}, /* move left  */
-    {  MOD4|CONTROL,     XK_j,          moveresize,        {.v = (int []){   0,   0,   0, 20 }}}, /* height grow   */
-    {  MOD4|CONTROL,     XK_k,          moveresize,        {.v = (int []){   0,   0,   0,-20 }}}, /* height shrink */
-    {  MOD4|CONTROL,     XK_l,          moveresize,        {.v = (int []){   0,   0,  20,   0 }}}, /* width grow    */
-    {  MOD4|CONTROL,     XK_h,          moveresize,        {.v = (int []){   0,   0, -20,   0 }}}, /* width shrink  */
-       DESKTOPCHANGE(    XK_1,                             0)
-       DESKTOPCHANGE(    XK_2,                             1)
-       DESKTOPCHANGE(    XK_3,                             2)
-       DESKTOPCHANGE(    XK_4,                             3)
-       DESKTOPCHANGE(    XK_5,                             4)
-       MONITORCHANGE(    XK_1,                             0)
-       MONITORCHANGE(    XK_2,                             1)
+    {  MOD4,             XK_j,          moveresize,        {.v = (int []){   0,  25,   0,   0 }}}, /* move down  */
+    {  MOD4,             XK_k,          moveresize,        {.v = (int []){   0, -25,   0,   0 }}}, /* move up    */
+    {  MOD4,             XK_l,          moveresize,        {.v = (int []){  25,   0,   0,   0 }}}, /* move right */
+    {  MOD4,             XK_h,          moveresize,        {.v = (int []){ -25,   0,   0,   0 }}}, /* move left  */
+    {  MOD4|SHIFT,       XK_j,          moveresize,        {.v = (int []){   0,   0,   0,  25 }}}, /* height grow   */
+    {  MOD4|SHIFT,       XK_k,          moveresize,        {.v = (int []){   0,   0,   0, -25 }}}, /* height shrink */
+    {  MOD4|SHIFT,       XK_l,          moveresize,        {.v = (int []){   0,   0,  25,   0 }}}, /* width grow    */
+    {  MOD4|SHIFT,       XK_h,          moveresize,        {.v = (int []){   0,   0, -25,   0 }}}, /* width shrink  */
+       DESKTOPCHANGE(    XK_F1,                             0)
+       DESKTOPCHANGE(    XK_F2,                             1)
+       DESKTOPCHANGE(    XK_F3,                             2)
+       DESKTOPCHANGE(    XK_F4,                             3)
+       MONITORCHANGE(    XK_F1,                             0)
+       MONITORCHANGE(    XK_F2,                             1)
 };
 
 /**
